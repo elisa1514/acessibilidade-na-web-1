e95f3cb5b264063187578ea8c62e1a7e1f45beae
elisa1514 <elisa.chalito@escola.pr.gov.br>
2025-04-02 14:20:22 +0000

    olha

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-//Código omitido
+document.addEventListener('DOMContentLoaded', function(){
+    const aumentaFonteBotao document.getElementById('aumentar-fonte');
+
+    let tamanhoAtualFonte = 1;
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+    })
+})
 
 <body>
     <header class=" p-5">
