f1a536d05ea6956341e031ca7566b3fd18a15ad6
elisa1514 <elisa.chalito@escola.pr.gov.br>
2025-04-02 14:25:15 +0000

    oi

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(){
-    const aumentaFonteBotao document.getElementById('aumentar-fonte');
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
 
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
@@ -14,8 +14,8 @@ document.addEventListener('DOMContentLoaded', function(){
 
 //Código omitido
 document.addEventListener('DOMContentLoaded', function(){
-    const aumentaFonteBotao document.getElementById('aumentar-fonte');
-    const diminuiFonteBotao document.getElementById('diminuir-fonte');
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
